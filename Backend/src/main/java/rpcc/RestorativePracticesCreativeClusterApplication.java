db6fbf0a680d26fa5312c1812c833f35f2ae2dbf
Jakov Jakovac <44674613+OffCrazyFreak@users.noreply.github.com>
2024-01-11 03:17:02 +0100

    Update RestorativePracticesCreativeClusterApplication.java

--- Backend/src/main/java/rpcc/RestorativePracticesCreativeClusterApplication.java
+++ Backend/src/main/java/rpcc/RestorativePracticesCreativeClusterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestorativePracticesCreativeClusterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestorativePracticesCreativeClusterApplication.class, args);
+		SpringApplication.run(RestorativePracticesCreativeClusterApplication.class, args); 
 	}
 
 }
