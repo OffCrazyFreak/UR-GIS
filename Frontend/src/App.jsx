e20c19bbba0dab02cd197f05e45026f83c65af6c
Jakov Jakovac <44674613+OffCrazyFreak@users.noreply.github.com>
2024-01-11 02:57:21 +0100

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -305,7 +305,7 @@ export default function App() {
         {/* TODO: DEPLOY */}
 
         <Typography variant="h2" align="center">
-          Restorative Practices Creative Clusteraaaaaaaa
+          Restorative Practices Creative Cluster
         </Typography>
 
         <Box
